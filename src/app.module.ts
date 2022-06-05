f639299173ce6bf59a84da2d2ed9cba54c850894
Codeliner <codeliner96@gmail.com>
2022-06-05 19:06:55 +0900

    Add Joi for validation environment of started project.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { join } from 'path';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 const envFilePath = `.env.${process.env.NODE_ENV}`;
 
@@ -13,6 +14,9 @@ const envFilePath = `.env.${process.env.NODE_ENV}`;
     ConfigModule.forRoot({
       envFilePath,
       ignoreEnvFile: process.env.NODE_ENV === 'production',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('local', 'development', 'production'),
+      }),
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
